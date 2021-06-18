884073e5cce3490ce1d6de536b09afea6385305d
leehungtien <leehungtien@gmail.com>
2021-06-18 23:35:46 +0800

    Added function to print out latest four news articles

--- Prediction_FINVIZ.py
+++ Prediction_FINVIZ.py
@@ -9,7 +9,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 finviz_url = 'https://finviz.com/quote.ashx?t='
 
-news_tables = {}
+# news_tables = {}
 
 # Get company from user
 ticker = input("Please enter company's ticker symbol: ")
@@ -19,9 +19,27 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
 }
 response = requests.get(url, headers=HEADERS)
-html = BeautifulSoup(response.text)
+html = BeautifulSoup(response.text, features="html.parser")
+
+def print_Headlines(coy_tr):
+    for index, table_row in enumerate(coy_tr):
+        headlines = table_row.a.text
+        datetime = table_row.td.text
+        print(headlines)
+        print(datetime)
+        
+        # Print out the 4 latest news articles
+        if index == 4:
+            break
+
 news_table = html.find(id='news-table')
-news_tables[ticker] = news_table
+coy_tr = news_table.findAll('tr')
+# news_tables[ticker] = news_table
+# company = news_tables[ticker]
+
+
+
+
 
 
 
